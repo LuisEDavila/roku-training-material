24e18591f58b76851bd6c6a44dea2d9fbd976257
LuisEDavila <luis.davila@accedo.tv>
2021-03-31 02:00:11 -0600

    Add lesson 4.5 Video Node

---
+++ 5.-Developer-Tools/src/App.js
@@ -0,0 +1,17 @@
+import { Deck, Slide, Heading } from 'spectacle';
+import './App.css';
+
+function App() {
+    return (
+        <Deck>
+            <Slide>
+                <Heading>Roku Developer Tools</Heading>
+            </Slide>
+            <Slide>
+                <Heading>Roku</Heading>
+            </Slide>
+        </Deck>
+    );
+}
+
+export default App;
